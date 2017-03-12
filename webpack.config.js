d4df8b3b19053d55fcbf24f6fba8cb7a00094deb
caoquang <caoduy1293@gmail.com>
2017-03-13 00:02:50 +0700

    First time to create app

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var config = {
+   entry: './app/components/Main.js',
+	
+   output: {
+      filename: 'public/bundle.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
